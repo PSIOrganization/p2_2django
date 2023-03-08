3050ad2e35ad9f62e591c33ed997612de64e423b
manuloseta <manuloseta@gmail.com>
2023-03-08 17:28:02 +0100

    final details for render part 2

--- persona/api/models.py
+++ persona/api/models.py
@@ -3,17 +3,16 @@ from django.db import models
 class Persona(models.Model):
     """Model representing a person."""
     # persona(id, nombre, apellido, email)
-    person_id = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
 
     class Meta:
-        ordering = ['person_id'] # lower person_id first
+        ordering = ['id'] # lower id first
 
     def get_absolute_url(self): # TO_DO
         """Returns the URL to access a particular person instance."""
-        return reverse('author-detail', args=[str(self.id)])
+        return reverse('personas', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""

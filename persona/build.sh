24168fe560e291b65e6b2a3bcbdf8d271ac96a5d
manuloseta <manuloseta@gmail.com>
2023-03-10 10:32:38 +0100

    mini commit 3

--- persona/build.sh
+++ persona/build.sh
@@ -8,4 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 # only populate the catalog the first time
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alumnodb', 'alumnodb@uam.es', 'alumnodb')"
+# python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('alumnodb', 'alumnodb@uam.es', 'alumnodb')"
